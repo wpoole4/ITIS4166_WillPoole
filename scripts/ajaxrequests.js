bd65fdd4687d278e505ff49fa9b4b9f75a9be6d5
William Poole <wpoole4@uncc.edu>
2023-01-25 18:41:31 -0500

    Updated styles/footer/header

---
+++ scripts/ajaxrequests.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $("#nav_list li").click(function (){
+        let name = $(this).children("a").attr("title");
+        let fileName = name + ".json";
+        $.ajax( {
+            type: "get",
+            url: fileName,
+            dataType: "json",
+            success: function(data){
+
+            $("aside > h4").html(data.accessories[0].name);
+            $("aside > p").html(data.accessories[0].bio);
+        }
+        });
+    });
+    });
